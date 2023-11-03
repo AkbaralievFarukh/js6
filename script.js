ef22e0336cf9054c8a517bcb3f4d1848bd043c17
Farukh <akbfarukh@gmail.com>
2023-11-03 06:00:20 +0600

    modifided

--- script.js
+++ script.js
@@ -15,8 +15,32 @@ console.log(cleanArray(arr))
 // Описание задачи: Напишите функцию, которая убирает повторяющиеся значения.
 arr = [1, 2, 3, 1, 2]
 let arrCount = {}
-for (let i of arr) {
-    arrCount[i] = arrCount[i] ? arrCount[i] + 1 : 1
+function arrClearSameValue() {
+    for (let i of arr) {
+        arrCount[i] = arrCount[i] ? arrCount[i] + 1 : 1
+    }
+    return arrCount
 }
-let result = Object.keys(arrCount).filter((i => arrCount[i] >= 1))
+let result = Object.keys(arrClearSameValue()).filter((i => arrCount[i] >= 1))
 console.log(result)
+
+// Задача. Даны два массива: ['a', 'b', 'c'] и [1, 2, 3]. Объедините их вместе.
+function combineArray(a, b) {
+    return a.concat(b)
+}
+console.log(combineArray(['a', 'b', 'c'], [1, 2, 3]))
+
+// Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6.
+function addEndValueArray(a) {
+    a.push(4, 5, 6)
+    return a
+}
+console.log(addEndValueArray([1, 2 ,3]))
+
+//  Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.
+function addStartValueArray(a) {
+    a.unshift(4, 5, 6)
+    return a
+}
+
+console.log(addStartValueArray([1, 2, 3]))
